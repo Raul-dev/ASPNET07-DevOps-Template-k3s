d950077987f9a73725101aa2b76da31875987875
raul <Raul.Averianov@dataart.com>
2023-04-24 17:22:39 +0300

    Added helm chart

---
+++ deployment/kuber/prepare.sh
@@ -0,0 +1,4 @@
+
+cp ../srv/filebeatjson/filebeat.yml /srv/filebeatjson/filebeat.yml
+cp ../srv/sso/realm.json  /srv/sso/realm.json
+cp ../srv/nginx/templates/default.conf.template /srv/nginx/templates/k3s/default.conf.template
